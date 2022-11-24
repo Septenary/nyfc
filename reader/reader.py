3aefc6ab3cddd1d01563a947a348d2b58568ee72
RKM <>
2022-11-24 18:58:31 -0500

    magic branch

--- reader/reader.py
+++ reader/reader.py
@@ -2,7 +2,7 @@ from pirc522 import RFID
 import threading
 
 
-class Reader(threading.Thread): 
+class Reader(threading.Thread):
     daemon = True
     latest_uid = []
     latest_data = [[]]
@@ -10,11 +10,10 @@ class Reader(threading.Thread):
     def __init__(self):
         super().__init__()
         self.rfid = RFID()
-        self.util = self.rfid.util()
+        # self.rfid.cleanup()
 
     def run(self):
         rfid = self.rfid
-
         # pirc522 read loop
         while True:
             rfid.wait_for_tag()
@@ -27,18 +26,23 @@ class Reader(threading.Thread):
 
     def read_card(self):
         rfid = self.rfid
+        util = rfid.util()
         uid, data = [], []
         (error, tag_type) = rfid.request()
         if not error:
             (error, uid_raw) = rfid.anticoll()
             if not error:
-                uid = ':'.join('{:03d}'.format(a) for a in uid_raw)
+                uid = ":".join("{:03d}".format(a) for a in uid_raw)
                 if not rfid.select_tag(uid_raw):
-                    for ii in range(0, 64):
-                        if not rfid.card_auth(rfid.auth_a, ii, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], uid_raw):
+                    for ii in range(0, 12):
+                        if not rfid.card_auth(
+                            rfid.auth_a,
+                            ii,
+                            [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF],
+                            uid_raw,
+                        ):
                             data.append(rfid.read(ii)[1])
+
                     rfid.stop_crypto()
 
-        if len(data) != 64:
-            data = []
         return uid, data
