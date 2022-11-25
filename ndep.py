040e22d36ceb06f758c05478ca4a22472b6a6052
RKM <>
2022-11-25 00:55:42 -0500

    Logging and command line arguments

--- ndep.py
+++ ndep.py
@@ -1,15 +1,25 @@
-import nfc, time
+#!/usr/bin/env python
+
+import time
+import logging
+import sys
+
+import nfc
 
 
 def on_startup(targets):
-    started = time.asctime()
-    print("Started: " + str(started))
+
+    if "--verbose" in sys.argv:
+        started = time.asctime()
+        print("Started: " + str(started))
     return targets
 
 
 def on_discover(target):
-    discovered = time.asctime()
-    print("Discovered: " + str(discovered))
+
+    if "--verbose" in sys.argv:
+        discovered = time.asctime()
+        print("Discovered: " + str(discovered))
     return True
 
 
@@ -23,14 +33,18 @@ and return True for requesting a presence loop that makes clf.connect() return o
 
 
 def on_connect(tag):
-    connected = time.asctime()
-    print("Connected: " + str(connected))
+
+    if "--verbose" in sys.argv:
+        connected = time.asctime()
+        print("Connected: " + str(connected))
     return lambda tag: False
 
 
 def on_release(tag):
-    released = time.asctime()
-    print("Connected: " + str(released))
+
+    if "--verbose" in sys.argv:
+        released = time.asctime()
+        print("Connected: " + str(released))
     # The tag object may be used for cleanup actions but not for communication.
     return tag
 
@@ -42,21 +56,33 @@ rdwr_options = {
     "on-connect": on_connect,
 }
 
-with nfc.ContactlessFrontend("udp") as clf:
-    print("Device found: " + str(clf))
-    tag = clf.connect(rdwr=rdwr_options)
-    if tag:
-        print(tag)
-        if tag.ndef:
-            assert tag.ndef is not None and tag.ndef.length > 0
-            # assert tag.ndef.records[0].type == 'urn:nfc:wkt:T'
-            assert tag.ndef.has_changed is False
-            print(tag.ndef.message.pretty())
-            for record in tag.ndef.records:
-                print(record)
+
+def main():
+
+    if "--debug" in sys.argv:
+        loglevel = logging.DEBUG - (1 if "--verbose" in sys.argv else 0)
+        logging.getLogger("nfc.clf").setLevel(loglevel)
+
+    with nfc.ContactlessFrontend("udp") as clf:
+        tag = clf.connect(rdwr=rdwr_options)
+        if tag:
+            print(tag)
+            if tag.ndef:
+                assert tag.ndef is not None and tag.ndef.length > 0
+                # assert tag.ndef.records[0].type == 'urn:nfc:wkt:T'
+                assert tag.ndef.has_changed is False
+                print(tag.ndef.message.pretty())
+                for record in tag.ndef.records:
+                    print(record)
+
 
 """
 The tag.ndef.records attribute contains a list of NDEF Records decoded from tag.ndef.octets with the ndeflib package. 
 Each record has common and type-specific methods and attributes for content access.
 """
 # print(record.type + record.uri)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(format="%(relativeCreated)d ms [%(name)s] %(message)s")
+    main()
